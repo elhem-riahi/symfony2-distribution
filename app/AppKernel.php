97d800fe85c108911bd962ec2c193c13d7614291
ADALA khaled <khaled.adala@gmail.com>
2014-01-23 17:39:34 +0100

    fix bundle fos user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,11 +13,14 @@ class AppKernel extends Kernel {
 				new Doctrine\Bundle\DoctrineBundle\DoctrineBundle (),
 				new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle (),
 				// DoctrineFixturesBundle
-				new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+				new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle (),
 				// StofDoctrineExtensionsBundle
-				new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+				new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle (),
 				// JMSTranslationBundle
-				new JMS\TranslationBundle\JMSTranslationBundle() 
+				new JMS\TranslationBundle\JMSTranslationBundle (),
+				// FOSUserBundle
+				new FOS\UserBundle\FOSUserBundle (),
+				new Application\UserBundle\ApplicationUserBundle () 
 		);
 		
 		if (in_array ( $this->getEnvironment (), array (
