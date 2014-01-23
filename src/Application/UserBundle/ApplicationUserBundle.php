5f7ae4a4dabbfa2ebb8ac0144b1ebf920c785d7f
ADALA khaled <khaled.adala@gmail.com>
2014-01-23 17:56:38 +0100

    fix bundle fos user

--- src/Application/UserBundle/ApplicationUserBundle.php
+++ src/Application/UserBundle/ApplicationUserBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApplicationUserBundle extends Bundle
 {
+	/**
+	 * extends FOSUserBundle
+	 */
+	public function getParent()
+	{
+		return 'FOSUserBundle';
+	}
 }
